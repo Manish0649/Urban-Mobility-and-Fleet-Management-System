d620210deebe38ad85ad54af78df7364eccc1272
Manish0649 <bansalmanish392@gmail.com>
2026-08-04 12:17:16 +0530

    UC2: Encapsulation and Security

--- main.py
+++ main.py
@@ -3,13 +3,16 @@ from vehicle import Vehicle
 def main():
 
     print("Welcome to the Urban Mobility and Fleet Management System")
+
     # Create a Vehicle object
     vehicle1 = Vehicle(vehicle_id="V001", model="Tesla", battery_percentage=85)
 
     # Print vehicle details
-    print(f"Vehicle ID: {vehicle1.vehicle_id}")
-    print(f"Model: {vehicle1.model}")
-    print(f"Battery Percentage: {vehicle1.battery_percentage}%")
+    print(vehicle1.get_battery_percentage())
+    vehicle1.set_maintenance_status("all good")
+    print(vehicle1.get_maintenance_status())
+    vehicle1.set_rental_price(50)
+    print(vehicle1.get_rental_price())
 
 if __name__ == "__main__":
     main()
--- vehicle.py
+++ vehicle.py
@@ -2,6 +2,29 @@ class Vehicle:
     def __init__(self, vehicle_id, model, battery_percentage):
         self.vehicle_id = vehicle_id
         self.model = model
-        self.battery_percentage = battery_percentage
+        self.battery_percentage = None
+        self.set_battery_percentage(battery_percentage)
 
-        
+    def set_battery_percentage(self, battery_percentage):
+        if 0 <= battery_percentage <= 100:
+            self.battery_percentage = battery_percentage
+        else:
+            raise ValueError("Battery percentage must be between 0 and 100.")
+
+    def get_battery_percentage(self):
+        return self.battery_percentage
+
+    def set_maintenance_status(self, status):
+        self.__maintenance_status = status
+
+    def get_maintenance_status(self):
+        return self.__maintenance_status
+
+    def set_rental_price(self, price):
+        if price >= 0:
+            self.__rental_price = price  
+        else:
+           raise ValueError("Rental price must be a non-negative value.")
+
+    def get_rental_price(self):
+        return self.__rental_price
